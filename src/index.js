d627431a90a18d0022cd4b1d43b86afb19f07985
wangjunying <wjy01082200@antgroup.com>
2021-08-13 17:57:41 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const traverse = require("@babel/traverse").default;
 const t = require("@babel/types");
 const core = require("@babel/core");
 const fs = require("fs");
-// 同步读取文件
-let sourceCode=fs.readFileSync('./encode.js','utf8');   
-
+// 同步读取文件 sources
+let sourceCode=fs.readFileSync('./sources.js','utf8');   
 const ast = parser.parse(sourceCode, {
     sourceType: "module",
     plugins: [
@@ -15,101 +14,96 @@ const ast = parser.parse(sourceCode, {
 });
 // var countMap = `{ __global__coverage__[startLine]: count }`
 let countMap;
-let __global__coverage__;
+let __coverage__;
 traverse(ast, {
     Program: {
-        enter(path, state) {
-            // init 
-            countMap = {};
-        },
-        exit(path, state) {
-            // global.__global__coverage__ = global.__global__coverage__ || {};
-            const body = path.get('body')[0];
-            var code = t.expressionStatement(
-                t.assignmentExpression(
-                    '=',
-                    t.memberExpression(
-                        t.identifier(
-                            'global'
-                        ),
-                        t.identifier(
-                            '__global__coverage__'
-                        ),
-                    ),
-                    t.logicalExpression(
-                        '||',
-                        t.memberExpression(
-                            t.identifier(
-                                'global'
-                            ),
-                            t.identifier(
-                                '__global__coverage__'
-                            ),
-                        ),
-                        t.objectExpression(
-                            []
-                        )
-                    )
-                )
-            );
-            body.insertBefore(code);
-            // __global__coverage__[line] = count;
-            Object.keys(countMap).forEach(line => {
+            enter(path, state) {
+                // init 
+                countMap = {};
+            },
+            exit(path, state) {
+                const body = path.get('body')[0];
+                // global.__global__coverage__ = global.__global__coverage__ || {};
                 var code = t.expressionStatement(
                     t.assignmentExpression(
                         '=',
                         t.memberExpression(
                             t.identifier(
-                                '__global__coverage__'
+                                'global'
                             ),
                             t.identifier(
-                                line
+                                '__coverage__'
                             ),
-                            true,
                         ),
-                        t.numericLiteral(
-                            countMap[line]-1
+                        t.logicalExpression(
+                            '||',
+                            t.memberExpression(
+                                t.identifier(
+                                    'global'
+                                ),
+                                t.identifier(
+                                    '__coverage__'
+                                ),
+                            ),
+                            t.objectExpression(
+                                []
+                            )
                         )
                     )
                 );
                 body.insertBefore(code);
-            });
-        }
-    },
-    Statement: {
-        exit(path) {
-            const node = path.node;
-            const loc = node.loc;
-            const startLine = loc?.start?.line;
-            if (startLine && !countMap[startLine]) {
-                if (!countMap[startLine]) {
-                    countMap[startLine] = 0;
-                }
-                const countExpr = t.expressionStatement(
-                    t.updateExpression(
-                        "++",
-                        t.memberExpression(
-                            t.identifier(
-                                '__global__coverage__'
+                // __global__coverage__[line] = count;
+                Object.keys(countMap).forEach(line => {
+                    var code = t.expressionStatement(
+                        t.assignmentExpression(
+                            '=',
+                            t.memberExpression(
+                                t.identifier(
+                                    '__coverage__'
+                                ),
+                                t.identifier(
+                                    line
+                                ),
+                                true,
                             ),
                             t.numericLiteral(
-                                startLine
-                            ),
-                            true,
-                        ),
-                    )
-                );
-                countMap[startLine]++;
-                path.insertBefore(countExpr);
+                                countMap[line]-1
+                            )
+                        )
+                    );
+                    body.insertBefore(code);
+                });
             }
-        },
     },
-    Function: {
-        exit(path) { 
-            // console.log(path.node)
-        }
+    Statement: {
+            exit(path) {
+                const node = path.node;
+                const loc = node.loc;
+                const startLine = loc?.start?.line;
+                if (startLine && !countMap[startLine]) {
+                    if (!countMap[startLine]) {
+                        countMap[startLine] = 0;
+                    }
+                    // __global__coverage__[line]++;
+                    const countExpr = t.expressionStatement(
+                        t.updateExpression(
+                            "++",
+                            t.memberExpression(
+                                t.identifier(
+                                    '__coverage__'
+                                ),
+                                t.numericLiteral(
+                                    startLine
+                                ),
+                                true,
+                            ),
+                        )
+                    );
+                    countMap[startLine]++;
+                    path.insertBefore(countExpr);
+                }
+            },
     },
 });
 const code = core.transformFromAstSync(ast, null, { configFile: false }).code;
-// console.log(code)
-fs.writeFileSync('./encode.js',code)
+fs.writeFileSync('./sources-test.js', code)
